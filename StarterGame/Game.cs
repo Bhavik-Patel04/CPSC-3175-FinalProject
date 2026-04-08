56ee9ca0b74a151889902037267e785ff0b25a0e
StevenAndrews-II <trey-94@hotmail.com>
2026-04-07 21:12:59 -0400

    update - mapgen

--- StarterGame/Game.cs
+++ StarterGame/Game.cs
@@ -12,67 +12,18 @@ namespace StarterGame
         private Player _player;
         private Parser _parser;
         private bool _playing;
+        private MapGenerator mapGenerator;
 
         public Game()
         {
-            _playing = false;
-            _parser = new Parser(new CommandWords());
-            _player = new Player(CreateWorld());
+            _playing        = false;
+            mapGenerator    = new MapGenerator();
+            _parser         = new Parser(new CommandWords());
+            _player         = new Player(mapGenerator.Generate());
         }
 
         // This creates a very simple world based 
         // on the landscape of Columbus State University
-        public Room CreateWorld()
-        {
-
-            
-
-            // outside west to bulivard
-            //
-
-
-
-            Room outside = new Room("the main entrance of the university", "outside");
-            Room scctparking = new Room("the parking lot at SCCT");
-            Room boulevard = new Room("the boulevard", "on");
-            Room universityParking = new Room("the parking lot at University Hall");
-            Room parkingDeck = new Room("the parking deck");
-            Room scct = new Room("the SCCT building");
-            Room theGreen = new Room("the green in from of Schuster Center");
-            Room universityHall = new Room("University Hall");
-            Room schuster = new Room("the Schuster Center");
-
-            outside.SetExit("west", boulevard);
-                 // link
-            boulevard.SetExit("east", outside);
-            boulevard.SetExit("south", scctparking);
-            boulevard.SetExit("west", theGreen);
-            boulevard.SetExit("north", universityParking);
-
-            scctparking.SetExit("west", scct);
-            scctparking.SetExit("north", boulevard);
-
-            scct.SetExit("east", scctparking);
-            scct.SetExit("north", schuster);
-
-            schuster.SetExit("south", scct);
-            schuster.SetExit("north", universityHall);
-            schuster.SetExit("east", theGreen);
-
-            theGreen.SetExit("west", schuster);
-            theGreen.SetExit("east", boulevard);
-
-            universityHall.SetExit("south", schuster);
-            universityHall.SetExit("east", universityParking);
-
-            universityParking.SetExit("south", boulevard);
-            universityParking.SetExit("west", universityHall);
-            universityParking.SetExit("north", parkingDeck);
-
-            parkingDeck.SetExit("south", universityParking);
-
-            return outside;
-        }
 
     /**
      *  Main play routine.  Loops until end of play.
--- StarterGame/MapGenerator.cs
+++ StarterGame/MapGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StarterGame;
+using System;
+using System.CodeDom.Compiler;
 
 public class MapGenerator
 {
@@ -8,6 +10,50 @@ public class MapGenerator
 	{
 	}
 
+
+	public Room Generate()
+	{
+
+        Room outside            = new Room("the main entrance of the university", "outside");
+        Room scctparking        = new Room("the parking lot at SCCT");
+        Room boulevard          = new Room("the boulevard", "on");
+        Room universityParking  = new Room("the parking lot at University Hall");
+        Room parkingDeck        = new Room("the parking deck");
+        Room scct               = new Room("the SCCT building");
+        Room theGreen           = new Room("the green in from of Schuster Center");
+        Room universityHall     = new Room("University Hall");
+        Room schuster           = new Room("the Schuster Center");
+
+        outside.SetExit("west", boulevard);
+
+        boulevard.SetExit("east", outside);
+        boulevard.SetExit("south", scctparking);
+        boulevard.SetExit("west", theGreen);
+        boulevard.SetExit("north", universityParking);
+
+        scctparking.SetExit("west", scct);
+        scctparking.SetExit("north", boulevard);
+
+        scct.SetExit("east", scctparking);
+        scct.SetExit("north", schuster);
+
+        schuster.SetExit("south", scct);
+        schuster.SetExit("north", universityHall);
+        schuster.SetExit("east", theGreen);
+
+        theGreen.SetExit("west", schuster);
+        theGreen.SetExit("east", boulevard);
+
+        universityHall.SetExit("south", schuster);
+        universityHall.SetExit("east", universityParking);
+
+        universityParking.SetExit("south", boulevard);
+        universityParking.SetExit("west", universityHall);
+        universityParking.SetExit("north", parkingDeck);
+
+        parkingDeck.SetExit("south", universityParking);
+        return outside;
+    }
 
 
 
