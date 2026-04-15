bf781559dde58b8451ab37bd2b8dbc6d554be2b0
StevenAndrews-II <trey-94@hotmail.com>
2026-04-15 18:49:28 -0400

    Pushed code - third word parser

--- StarterGame/TalkCommand.cs
+++ StarterGame/TalkCommand.cs
@@ -37,6 +37,7 @@ namespace StarterGame
 
             if (this.HasSecondWord())
             {
+                
                 // this would be the destination of the conversation < - then there needs to be a dialog option lol 
 
                 return false;
